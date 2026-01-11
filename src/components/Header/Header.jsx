c200b94c18ad38cada96f9d0928707ceb08579cd
Carlos <carlosaac232001@gmail.com>
2026-01-10 22:07:09 -0500

    feat: create the basic Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,24 @@
+import { Link } from 'react-router';
+import './Header.css';
+
 export default function Header() {
-  return <p>Header</p>;
+  return (
+    <header className='header'>
+      <h1>Page Logo</h1>
+
+      <nav>
+        <ul className='header__links'>
+          <Link to='/TOP-shopping-cart/' className='header__link'>
+            Home
+          </Link>
+          <Link to='shop' className='header__link'>
+            Shop
+          </Link>
+          <Link to='cart' className='header__link'>
+            Cart
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 }
