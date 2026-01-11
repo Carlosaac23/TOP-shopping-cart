8f3f115bf642697cd17722abdc1cf9332efb8118
Carlos <carlosaac232001@gmail.com>
2026-01-10 21:14:43 -0500

    chore: add main components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import './styles/index.css';
+import './styles/reset.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className='main'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  </StrictMode>
+);
