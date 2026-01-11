da826df0e449c6e64238dc4a7411bddccba1b55a
Carlos <carlosaac232001@gmail.com>
2026-01-10 22:06:19 -0500

    feat: create the basic Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className='footer'>
-      <h2>Footer</h2>
+      <p className='footer_text'>
+        <a
+          className='footer_author'
+          href='https://github.com/Carlosaac23'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Carlos Acosta
+        </a>{' '}
+        {new Date().getFullYear()} &copy; Rights Reservded
+      </p>
     </footer>
   );
 }
