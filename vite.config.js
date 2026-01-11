3e33cc10b35488467ec2086caac3672f0fb67438
Carlos <carlosaac232001@gmail.com>
2026-01-10 21:20:34 -0500

    feat: add alias config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/TOP-shopping-cart/',
+  resolve: {
+    alias: {
+      '@': path.resolve('./src/components'),
+    },
+  },
 });
