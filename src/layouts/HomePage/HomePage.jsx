1d15b0fb05a7053d350544273149a61e4b130eeb
Carlos <carlosaac232001@gmail.com>
2026-01-12 14:55:43 -0500

    feat: - create useProducts hook to fetch products - use sonner to show errors - render the products in Product component

--- src/layouts/HomePage/HomePage.jsx
+++ src/layouts/HomePage/HomePage.jsx
@@ -1,11 +1,53 @@
-import { Link } from 'react-router';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import Product from '../../components/Product/Product';
+import { fetchProducts as products } from '../../lib/fetch-products';
+import './HomePage.css';
+
+function useProducts() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const data = await products();
+        setData(data);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        setData(null);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return { data, error };
+}
 
 export default function HomePage() {
+  const { data: products, error } = useProducts();
+  console.log(products);
+
+  if (error) {
+    toast.error(error);
+    return;
+  }
+
   return (
     <main>
-      <h1>Home Page</h1>
-      <Link to='shop'>Shop</Link>
-      <Link to='cart'>Cart</Link>
+      <h1 className='products__title'>Products</h1>
+      <div className='products__container'>
+        {products?.map(({ id, title, price, description }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+          />
+        ))}
+      </div>
     </main>
   );
 }
