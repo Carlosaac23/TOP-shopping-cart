a8032e8c83e413a169849feeccebe430062422d3
Carlos <carlosaac232001@gmail.com>
2026-01-12 16:04:56 -0500

    feat: render the important info into the card and add style to it

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,27 @@
+import { CirclePlus } from 'lucide-react';
+import { capitalize } from '../../utils';
 import './Product.css';
 
-export default function Product({ title, price, description }) {
+export default function Product({
+  image,
+  title,
+  category,
+  price,
+  rating: { count, rate },
+}) {
   return (
     <div className='product__card'>
-      <h2>{title}</h2>
-      <p>{price}</p>
-      <p>{description}</p>
+      <img className='product__card-image' src={image} alt='Product Image' />
+      <div className='product__card-top'>
+        <h2 className='product__card-title'>{title}</h2>
+        <CirclePlus size={28} color='#fb923c' />
+      </div>
+      <p className='product__card-category'>{capitalize(category)}</p>
+      <p className='product__card-price'>${price}</p>
+      <div className='product__card-bottom'>
+        <span className='product__card-rate'>Rating: {rate}</span>
+        <span className='product__card-count'>({count})</span>
+      </div>
     </div>
   );
 }
