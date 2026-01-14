e0b3b60173614acd1e604644fff9ce5bb2cd8c3f
Carlos <carlosaac232001@gmail.com>
2026-01-14 10:39:36 -0500

    chore: delete Footer folder with css file to keep only Footer component into components folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import './Footer.css';
-
 export default function Footer() {
   return (
-    <footer className='footer'>
-      <p className='footer_text'>
+    <footer className='mt-auto bg-orange-400 p-2'>
+      <p className='text-center text-sm'>
         <a
-          className='footer_author'
+          className='hover:underline hover:underline-offset-1'
           href='https://github.com/Carlosaac23'
           target='_blank'
           rel='noopener noreferrer'
