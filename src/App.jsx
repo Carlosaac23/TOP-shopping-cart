4f84852ccbe9d6f5281e29932dcd2b2a9d4cd751
Carlos <carlosaac232001@gmail.com>
2026-01-14 10:47:10 -0500

    fix: update components path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router';
 
-import Footer from '@/Footer/Footer';
-import Header from '@/Header/Header';
+import Footer from '@/Footer';
+import Header from '@/Header';
 
 export default function App() {
   return (
