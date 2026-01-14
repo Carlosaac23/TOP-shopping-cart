d153dba1b83c323b085780e63e670cb7da76c8a0
Carlos <carlosaac232001@gmail.com>
2026-01-14 10:46:41 -0500

    chore: delete Header folder and keep solo .jsx component into components folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { ShoppingBasket } from 'lucide-react';
+import { Link } from 'react-router';
+
+export default function Header() {
+  return (
+    <header className='flex items-center justify-between bg-orange-400 p-8 shadow-sm'>
+      <Link to='/TOP-shopping-cart/'>
+        <h1 className='text-3xl font-extrabold uppercase'>Shopping Cart</h1>
+      </Link>
+
+      <nav>
+        <ul className='flex gap-2'>
+          <Link
+            to='/TOP-shopping-cart/'
+            className='rounded-md px-4 py-2 hover:bg-orange-600 hover:text-orange-50'
+          >
+            Home
+          </Link>
+          <Link
+            to='shop'
+            className='rounded-md px-4 py-2 hover:bg-orange-600 hover:text-orange-50'
+          >
+            Shop
+          </Link>
+          <Link
+            to='cart'
+            className='rounded-md px-4 py-2 hover:bg-orange-600 hover:text-orange-50'
+          >
+            <ShoppingBasket />
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
