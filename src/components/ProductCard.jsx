fd10a5b54106b3ce93aff4d4abf2744bb7dbae01
Carlos <carlosaac232001@gmail.com>
2026-01-14 14:39:53 -0500

    style: - improve the scale animation when hover - add title attribute to each product title - truncate long titles and you can see them hover them - add cursor-pointer to add icon when hover

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,15 +8,24 @@ export default function ProductCard({
   rating: { count, rate },
 }) {
   return (
-    <div className='w-80 rounded-custom border border-neutral-300 p-4 shadow-sm transition-transform duration-150 ease-out hover:scale-[1.05]'>
+    <div className='w-80 rounded-custom border border-neutral-300 p-4 shadow-sm transition-transform duration-150 ease-out hover:scale-98'>
       <img
         className='h-40 w-full rounded-xl bg-neutral-200 object-contain p-4'
         src={image}
         alt='Product Image'
       />
       <div className='flex items-center justify-between py-4'>
-        <h2 className='w-[75%] text-orange-400'>{title}</h2>
-        <CirclePlus size={28} color='#fb923c' />
+        <h2
+          className='w-[75%] truncate text-orange-400 hover:text-wrap'
+          title={title}
+        >
+          {title}
+        </h2>
+        <CirclePlus
+          className='hover:cursor-pointer'
+          size={28}
+          color='#fb923c'
+        />
       </div>
       <p className='mb-2 text-xs text-neutral-500 uppercase'>{category}</p>
       <p className='text-xlg inline-block rounded-md bg-orange-400 px-3 py-1 font-bold'>
