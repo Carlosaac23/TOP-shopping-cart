259270b743eae821ad61de55652d21899b9d7198
Carlos <carlosaac232001@gmail.com>
2026-01-13 23:30:02 -0500

    feat: use tailwind to style HomePage component

--- src/layouts/HomePage/HomePage.jsx
+++ src/layouts/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className='py-4 text-center text-3xl font-bold'>Home Page</h1>
     </main>
   );
 }
