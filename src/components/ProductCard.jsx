718aaf04fa9b977fc2903d91440faad5ae08a84b
Carlos <carlosaac232001@gmail.com>
2026-01-14 11:31:03 -0500

    style: improve tailwind styles

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,21 +8,21 @@ export default function ProductCard({
   rating: { count, rate },
 }) {
   return (
-    <div className='w-100 rounded-[1.6rem] border border-neutral-300 p-6 shadow-sm transition-transform duration-150 ease-out hover:scale-[1.05]'>
+    <div className='w-80 rounded-custom border border-neutral-300 p-4 shadow-sm transition-transform duration-150 ease-out hover:scale-[1.05]'>
       <img
-        className='h-40 w-full rounded-md bg-neutral-200 object-contain p-4'
+        className='h-40 w-full rounded-xl bg-neutral-200 object-contain p-4'
         src={image}
         alt='Product Image'
       />
       <div className='flex items-center justify-between py-4'>
-        <h2 className='w-[75%] text-xl text-orange-400'>{title}</h2>
+        <h2 className='w-[75%] text-orange-400'>{title}</h2>
         <CirclePlus size={28} color='#fb923c' />
       </div>
-      <p className='mb-2 text-sm text-neutral-500 uppercase'>{category}</p>
-      <p className='inline-block rounded-md bg-orange-400 px-4 py-2 text-2xl font-bold'>
+      <p className='mb-2 text-xs text-neutral-500 uppercase'>{category}</p>
+      <p className='text-xlg inline-block rounded-md bg-orange-400 px-3 py-1 font-bold'>
         ${price}
       </p>
-      <div className='mt-3.5 flex gap-2'>
+      <div className='mt-3.5 flex gap-2 text-sm'>
         <span className='font-semibold'>Rating: {rate}</span>
         <span className='text-neutral-500'>({count})</span>
       </div>
