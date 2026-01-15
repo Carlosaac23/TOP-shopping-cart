e5fa64d59276b23f715e2780c1027aac9d305b65
Carlos <carlosaac232001@gmail.com>
2026-01-15 14:50:46 -0500

    feat: create a mockup for CartPage component

--- src/layouts/CartPage.jsx
+++ src/layouts/CartPage.jsx
@@ -1,7 +1,32 @@
 export default function CartPage() {
   return (
-    <main>
-      <h1>Cart Page</h1>
+    <main className='mx-10 flex justify-between'>
+      <div>
+        <h1 className='text-4xl font-bold tracking-tight'>My Shopping Cart</h1>
+        <p>Lis of Products:</p>
+      </div>
+      <div>
+        <h2>Summary</h2>
+        <div>
+          <p>Do you have promo code?</p>
+          <input type='text' name='' id='' />
+        </div>
+        <div>
+          <h3>Subtotal:</h3>
+          <p>Shipping:</p>
+          <p>Sales Tax:</p>
+          <p>Estimated Total:</p>
+          <button type='button'>Checkout</button>
+        </div>
+        <div>
+          <p>
+            Need help? Call us at{' '}
+            <a className='underline' href='tel:+573007410161'>
+              3007410161
+            </a>
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
