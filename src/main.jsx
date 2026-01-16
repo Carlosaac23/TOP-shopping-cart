b1c03a731634000b6ba4c7588c8d116ac5f7d380
Carlos <carlosaac232001@gmail.com>
2026-01-15 21:10:27 -0500

    feat: add redirect to my own ErrorPage component

--- src/main.jsx
+++ src/main.jsx
@@ -29,13 +29,24 @@ const router = createBrowserRouter([
         path: 'cart',
         element: <CartPage />,
       },
+      {
+        path: '*',
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
     <Toaster richColors />
   </StrictMode>
 );
+
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+  router.navigate(redirectPath.replace('/TOP-shopping-cart', ''));
+}
