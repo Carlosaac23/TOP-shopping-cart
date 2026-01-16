2a67524da42eb7d4576731112a11aef8344f5da1
Carlos <carlosaac232001@gmail.com>
2026-01-16 13:05:17 -0500

    style: center the spinner when fetching new product

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -15,7 +15,12 @@ export default function VaulDrawer({ productId, isOpen, onClose }) {
         <Drawer.Overlay className='fixed inset-0 bg-black/40' />
         <Drawer.Content className='fixed right-0 bottom-0 left-0 h-fit bg-gray-100 outline-none'>
           <div className='bg-orange-50 p-4'>
-            {isLoading && <BeatLoader color='#fb923c' />}
+            {isLoading && (
+              <BeatLoader
+                color='#fb923c'
+                cssOverride={{ textAlign: 'center' }}
+              />
+            )}
             {error && toast.error(error)}
             {product && <ProductVaul key={product.id} {...product} />}
           </div>
