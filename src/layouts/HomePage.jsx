27a67f6c474219672c96d0f1f3843104169459b1
Carlos <carlosaac232001@gmail.com>
2026-01-15 21:52:18 -0500

    refactor: import useProductDrawer and useFilteredProducts so keeps the component clean and only render UI

--- src/layouts/HomePage.jsx
+++ src/layouts/HomePage.jsx
@@ -1,61 +1,20 @@
-import { useEffect, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
 import { toast } from 'sonner';
 
 import VaulDrawer from '@/Drawer';
 import ProductCard from '@/ProductCard';
 
-import { fetchProducts } from '../lib/fetch-products';
-import { randomNumber } from '../utils';
-
-function useFilterProducts() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const filteredProducts = [];
-        const data = await fetchProducts();
-
-        while (filteredProducts.length < 3) {
-          const randomId = randomNumber(data.length);
-          const newProduct = data.find(product => product.id === randomId);
-          const isDuplicate = filteredProducts.some(
-            product => product.id === newProduct.id
-          );
-
-          if (!isDuplicate) {
-            filteredProducts.push(newProduct);
-          }
-        }
-
-        setData(filteredProducts);
-        setError(null);
-      } catch (error) {
-        setError(error.message);
-        setData(null);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
-
-  return { data, error, isLoading };
-}
+import { useFilteredProducts } from '../hooks/useFilteredProducts';
+import { useProductDrawer } from '../hooks/useProductDrawer';
 
 export default function HomePage() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [selectedProductId, setSelectedProductId] = useState(null);
-  const { data: filteredProducts, error, isLoading } = useFilterProducts();
-
-  function handleProductClick(productId) {
-    setIsDrawerOpen(true);
-    setSelectedProductId(productId);
-  }
+  const {
+    isDrawerOpen,
+    selectedProductId,
+    handleProductClick,
+    handleCloseDrawer,
+  } = useProductDrawer();
+  const { data: filteredProducts, error, isLoading } = useFilteredProducts();
 
   return (
     <main className='bg-orange-50 px-6 py-12'>
@@ -96,7 +55,7 @@ export default function HomePage() {
       <VaulDrawer
         isOpen={isDrawerOpen}
         productId={selectedProductId}
-        onClose={() => setIsDrawerOpen(false)}
+        onClose={handleCloseDrawer}
       />
     </main>
   );
