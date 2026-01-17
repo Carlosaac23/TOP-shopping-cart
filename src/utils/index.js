3563dceafda0955060fc528789fc1cf75e196fb8
Carlos <carlosaac232001@gmail.com>
2026-01-17 11:06:39 -0500

    feat: create fakeShippingCosts and fakeSalesTaxes functions to manage cart total

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,16 @@ function randomNumber(limitNumber) {
   return Math.floor(Math.random() * limitNumber + 1);
 }
 
-export { randomNumber };
+function fakeShippingCosts(subtotal) {
+  if (subtotal >= 1000) return 0;
+  if (subtotal < 1000 && subtotal > 700) return 15;
+  if (subtotal < 700 && subtotal > 400) return 22;
+  if (subtotal < 400 && subtotal > 150) return 27;
+  if (subtotal < 150) return 33;
+}
+
+function fakeSalesTaxes(subtotal) {
+  return subtotal * 0.19;
+}
+
+export { randomNumber, fakeShippingCosts, fakeSalesTaxes };
