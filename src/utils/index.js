b9f28e89824650f80716cb7372225acb10393224
Carlos <carlosaac232001@gmail.com>
2026-01-17 11:20:16 -0500

    fix: fakeShippingCosts return 0 as default

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,8 @@ function fakeShippingCosts(subtotal) {
   if (subtotal < 1000 && subtotal > 700) return 15;
   if (subtotal < 700 && subtotal > 400) return 22;
   if (subtotal < 400 && subtotal > 150) return 27;
-  if (subtotal < 150) return 33;
+  if (subtotal < 150 && subtotal > 1) return 33;
+  return 0;
 }
 
 function fakeSalesTaxes(subtotal) {
