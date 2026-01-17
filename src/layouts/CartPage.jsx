eef2dbce177858a1c31e28183d3261ec142f041d
Carlos <carlosaac232001@gmail.com>
2026-01-17 11:07:36 -0500

    feat: - import new fakeShippingCosts and fakeSalexTaxes functions to calculate all prices - create variables for each cost to make the code readable - use each variable in each paragraph tag to render the specific price

--- src/layouts/CartPage.jsx
+++ src/layouts/CartPage.jsx
@@ -1,10 +1,17 @@
 import ProductInfo from '@/ProductInfo';
 
 import { useCart } from '../contexts/CartContext';
+import { fakeShippingCosts, fakeSalesTaxes } from '../utils';
 
 export default function CartPage() {
   const { cartItems } = useCart();
-  console.log(cartItems);
+  const subtotal = cartItems.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+  const shippingCost = fakeShippingCosts(subtotal);
+  const salesTax = fakeSalesTaxes(subtotal);
+  const total = subtotal + shippingCost + salesTax;
 
   return (
     <main className='mx-10 flex justify-between gap-6'>
@@ -25,10 +32,30 @@ export default function CartPage() {
           <input type='text' name='' id='' />
         </div>
         <div>
-          <h3>Subtotal:</h3>
-          <p>Shipping:</p>
-          <p>Sales Tax:</p>
-          <p>Estimated Total:</p>
+          <h3>
+            Subtotal:{' '}
+            <span className='font-semibold'>${subtotal.toFixed(2)}</span>
+          </h3>
+          <p>
+            Shipping:{' '}
+            <span className='font-semibold'>
+              {subtotal === 0
+                ? '$0'
+                : shippingCost === 0
+                  ? 'Free'
+                  : `$${shippingCost.toFixed(2)}`}
+            </span>
+          </p>
+          <p>
+            Sales Tax:{' '}
+            <span className='font-semibold'>${salesTax.toFixed(2)}</span>
+          </p>
+          <p>
+            Estimated Total:{' '}
+            <span className='font-semibold'>
+              ${total === 33 ? '0' : total.toFixed(2)}
+            </span>
+          </p>
           <button type='button'>Checkout</button>
         </div>
         <div>
